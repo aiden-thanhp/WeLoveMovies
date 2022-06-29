a3090dc2d3bd983a4b7163d56b0878ee81067cb6
aiden-thanhp <102391447+aiden-thanhp@users.noreply.github.com>
2022-06-29 12:10:49 -0400

    changes to knexfile 2

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,16 @@
 const path = require("path");
 
-require("dotenv").config();
+require("dotenv").config({path: "./.env"});
 console.log(process.env)
-
-const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -20,7 +22,11 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME },
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
